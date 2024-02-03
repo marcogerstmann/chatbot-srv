367383a042f216a90b1fca82aa5b4ecf5c6e4ee4
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-03 17:23:54 +0100

    add first vector db call

--- app/services/faq.py
+++ app/services/faq.py
@@ -1,7 +1,27 @@
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+
 from app.backend.settings import settings
 
 
 def answer_question(question: str) -> str:
-    print(settings.openai_api_key)
-    print(question)
-    return "The answer is 42"
+    chat = ChatOpenAI(openai_api_key=settings.openai_api_key)
+    embeddings = OpenAIEmbeddings(openai_api_key=settings.openai_api_key)
+    db = Chroma(
+        persist_directory="./temp/emb",
+        embedding_function=embeddings
+    )
+
+    retriever = db.as_retriever()
+
+    chain = RetrievalQA.from_chain_type(
+        llm=chat,
+        retriever=retriever,
+        chain_type="stuff"
+    )
+
+    # TODO: Use chain.invoke instead of chain.run
+    result = chain.run(question)
+
+    return result
