5684da81209ceacaada71385c230386f96a96caa
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-06 15:00:48 +0100

    move to constants

--- app/constants.py
+++ app/constants.py
@@ -9,4 +9,8 @@ MIN_SOLAR_API_IMAGE_QUALITY: Final = "MEDIUM"  # "LOW" | "MEDIUM" | "HIGH"
 
 # Solar potential calculations
 DC_TO_AC_RATE = 0.85
-AVG_COST_PER_KWH = 0.26  # TODO: Move to database or external api, because it changes more frequently
+AVG_COST_PER_KWH = 0.26  # TODO: Move to DB or external api, because it changes more frequently
+
+# Google Drive Resources
+VECTOR_SOURCES_BASE_FOLDER_ID = "1Z7WKT8-iiOpuAibGjlFpz-fSnodYa1Sh"
+LEADS_GOOGLE_SPREADSHEET_ID = "15mpbPy9UQvnVZkWgXgSBYipHhgUC4uS7kMxZawqNvW8"  # TODO: Move to DB (customer specific)
--- app/services/leads_service.py
+++ app/services/leads_service.py
@@ -1,12 +1,11 @@
+from app.constants import LEADS_GOOGLE_SPREADSHEET_ID
 from app.schemas.lead_request_schema import LeadRequestSchema
 from app.services.google_api_service import capture_lead_to_google_spreadsheet
-
-leads_google_spreadsheet_id = "15mpbPy9UQvnVZkWgXgSBYipHhgUC4uS7kMxZawqNvW8"
 
 
 def capture_lead(lead: LeadRequestSchema) -> bool:
     capture_lead_to_google_spreadsheet(
-        leads_google_spreadsheet_id,
+        LEADS_GOOGLE_SPREADSHEET_ID,
         [lead.name, lead.address, lead.phone]
     )
     return True
