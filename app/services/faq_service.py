94d6a3bc16bdb1ec5db5c7ec6f330210590501b1
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-10 17:52:43 +0100

    refactor llms to own module

--- app/services/faq_service.py
+++ app/services/faq_service.py
@@ -1,13 +1,10 @@
 from langchain.chains import RetrievalQA
-from langchain_openai import ChatOpenAI
 
-from app.backend.settings import settings
+from app.backend.llms.chatopenai import chat
 from app.backend.vector_stores.pgvector_vector_store_base import vector_store_base
 
 
 def answer_question(question: str) -> str:
-    chat = ChatOpenAI(openai_api_key=settings.openai_api_key)
-
     chain = RetrievalQA.from_chain_type(
         llm=chat, retriever=vector_store_base.as_retriever(), chain_type="stuff"
     )
