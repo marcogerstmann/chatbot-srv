db736929e600b445308b114bfb4981751b2382f5
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-15 16:20:28 +0100

    refactor faq_service

--- app/routers/chat_router.py
+++ app/routers/chat_router.py
@@ -2,22 +2,22 @@ from fastapi import APIRouter
 
 from app.schemas.chat_request_schema import ChatRequestSchema
 from app.schemas.chat_response_schema import ChatResponseSchema
-from app.services.faq_service import answer_question, answer_simple_chat
+from app.services.chat_service import handle_conversation_prompt, handle_faq_question
 
 router = APIRouter(prefix="/chat")
 
 
 @router.post("", response_model=ChatResponseSchema)
 def simple_chat(request: ChatRequestSchema) -> ChatResponseSchema:
-    """Handle FAQ chat message"""
+    """Handle conversation prompt"""
 
-    answer = answer_simple_chat(request.input)
+    answer = handle_conversation_prompt(request.input)
     return ChatResponseSchema(output=answer)
 
 
 @router.post("/faq", response_model=ChatResponseSchema)
-def handle_faq_chat_message(request: ChatRequestSchema) -> ChatResponseSchema:
+def faq_question(request: ChatRequestSchema) -> ChatResponseSchema:
     """Handle FAQ chat message"""
 
-    answer = answer_question(request.input)
+    answer = handle_faq_question(request.input)
     return ChatResponseSchema(output=answer)
--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -1,18 +1,19 @@
 from langchain.chains import ConversationChain, RetrievalQA
+from langchain.memory import ConversationBufferMemory
 
 from app.backend.llms.chatopenai import chat
 from app.backend.vector_stores.pgvector_vector_store_base import vector_store_base
 
 
-def answer_simple_chat(question: str) -> str:
-    chain = ConversationChain(llm=chat)
+def handle_conversation_prompt(prompt: str) -> str:
+    chain = ConversationChain(llm=chat, verbose=True, memory=ConversationBufferMemory())
 
-    output = chain.predict(input=question)
+    output = chain.predict(input=prompt)
 
     return output
 
 
-def answer_question(question: str) -> str:
+def handle_faq_question(question: str) -> str:
     chain = RetrievalQA.from_chain_type(
         llm=chat, retriever=vector_store_base.as_retriever(), chain_type="stuff"
     )
