93f79f654773005629d098cc1a27cdd20d0800e9
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-17 15:23:27 +0100

    cleanup dependencies

--- app/constants.py
+++ app/constants.py
@@ -1,9 +1,9 @@
 from typing import Final
 
 # Custom http headers
-HTTP_HEADER_SESSION_ID = "x-session-id"
-HTTP_HEADER_CUSTOMER_ID = "x-customer-id"
+HTTP_HEADER_SESSION_ID: Final = "x-session-id"
+HTTP_HEADER_CUSTOMER_ID: Final = "x-customer-id"
 
 # Open API
-OPEN_API_TITLE: Final = "Solar Chatbot Service"
-OPEN_API_DESCRIPTION: Final = "API and backend for the solar industry chatbot."
+OPEN_API_TITLE: Final = '"hi ai" Chatbot Service'
+OPEN_API_DESCRIPTION: Final = 'API and backend for "hi ai" chatbots.'
