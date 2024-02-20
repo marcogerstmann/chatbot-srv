e05117f9421915ec587c425465c2dfe151724cea
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-20 11:12:27 +0100

    add first basic web scrapes with WebBaseLoader and beautifulsoup kwargs

--- app/services/db_service.py
+++ app/services/db_service.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 
+import bs4
 from fastapi import Depends
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import GoogleDriveLoader
+from langchain_community.document_loaders import GoogleDriveLoader, WebBaseLoader
 
 from app.backend.chat.vector_stores.pgvector_vector_store_base import vector_store
 from app.backend.settings import Settings, get_settings
@@ -13,9 +14,24 @@ class DbService:
         self.settings = settings
 
     def sync_embeddings(self) -> bool:
-        self.__clear_vector_store()
-        self.__add_documents_to_vector_store()
+        # self.__clear_vector_store()
+        # self.__add_documents_to_vector_store()
+        self.testWebLoading()
         return True
+
+    def testWebLoading(self):
+        loader = WebBaseLoader(
+            web_paths=[
+                "https://www.piercingline.com/product/Titan-Ohrstecker-KREIS/TSO107-184989",
+                "https://www.piercingline.com/product/Chirurgenstahl-Klemmkugel-KRISTALL/SLX800-93851",
+            ],
+            bs_kwargs=dict(
+                parse_only=bs4.SoupStrainer(class_=("product-detail-description-text"))
+            ),
+        )
+        docs = loader.load()  # TODO: Use loader.load_and_split() instead?
+        print(docs)
+        # vector_store.add_documents(docs)
 
     def __clear_vector_store(self):
         vector_store.delete_collection()
