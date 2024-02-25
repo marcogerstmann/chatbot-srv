466fa066bfbe148221b032df467edc1994d1a536
Marco Gerstmann <mail@marcogerstmann.com>
2024-02-25 16:29:36 +0100

    use ConversationBufferWindowMemory

--- app/backend/chat/memories/postgres_memory.py
+++ app/backend/chat/memories/postgres_memory.py
@@ -1,13 +1,14 @@
-from langchain.memory import ConversationBufferMemory, PostgresChatMessageHistory
+from langchain.memory import ConversationBufferWindowMemory, PostgresChatMessageHistory
 
 from app.backend.settings import get_settings
 
 
 def build_memory(session_id: str):
-    return ConversationBufferMemory(
+    return ConversationBufferWindowMemory(
         chat_memory=PostgresChatMessageHistory(
             connection_string=get_settings().postgres_connection_string,
             session_id=session_id,
         ),
+        k=5,
         return_messages=True,
     )
